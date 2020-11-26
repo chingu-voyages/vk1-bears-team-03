43f4d2bf0689f9223f02474292a76ba907de18d7
xanxan2 <xanneteneza@gmail.com>
2020-11-27 07:56:26 +0800

    First commit

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -4,8 +4,9 @@ import { CFooter } from '@coreui/react'
 const TheFooter = () => {
   return (
     <CFooter fixed={false}>
-      <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
+      
+    <div>
+        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">Asset Management System</a>
         <span className="ml-1">&copy; 2020 creativeLabs.</span>
       </div>
       <div className="mfs-auto">
