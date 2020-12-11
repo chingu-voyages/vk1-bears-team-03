a47e2511c8620410a6775d15c029988ea27181ba
xanxan2 <xanneteneza@gmail.com>
2020-12-11 21:44:33 +0800

    button in form

--- src/views/assets/createnewassets/CreateNewAssets.js
+++ src/views/assets/createnewassets/CreateNewAssets.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import {
-  CCardBody, CCol, CCard, CCardHeader,
+  CCardBody, CCol, CCard, CCardHeader, CFormGroup, CLabel, CInput, CSelect, CTextarea, CInputGroupText, CInputGroupAppend, CInputGroupPrepend, CInputGroup, CButton, CCardFooter
 
 } from '@coreui/react'
 
 import BackButtonAssets from 'src/views/assets/backbuttonassets/BackButtonAssets'
+// import CIcon from '@coreui/icons-react'
 
 
 const CreateNewAssets = () => {
     
-//   const [active, setActive] = useState(1)
-//   const lorem = ''
-
     return(
         
         <CCol xs="12" md="6" lg="12" className="mb-4">
@@ -19,10 +17,183 @@ const CreateNewAssets = () => {
           <CCardHeader>
 
           <BackButtonAssets/>
-
+          <CButton type="reset" size="sm" color="danger" className="mr-1"> Reset</CButton>
           </CCardHeader>
           <CCardBody>
-          
+          <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Asset Name</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="" />
+                  </CCol>
+                 
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Asset Tag</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="select">Status</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select</option>
+                      <option value="1">Option #1</option>
+                      <option value="2">Option #2</option>
+                      <option value="3">Option #3</option>
+                    </CSelect>
+                  </CCol>
+                  
+                </CFormGroup>
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="textarea-input">Description</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CTextarea 
+                      name="textarea-input" 
+                      id="textarea-input" 
+                      rows="2"
+                      placeholder="Content..." 
+                    />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Serial Number</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="select">Manufacturer</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select</option>
+                      <option value="1">Option #1</option>
+                      <option value="2">Option #2</option>
+                      <option value="3">Option #3</option>
+                    </CSelect>
+                  </CCol>
+                  
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Purchase Date</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                  <CInput type="date" id="date-input" name="date-input" placeholder="date" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="select">Supplier</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select</option>
+                      <option value="1">Option #1</option>
+                      <option value="2">Option #2</option>
+                      <option value="3">Option #3</option>
+                    </CSelect>
+                  </CCol>
+                  
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Invoice Number</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Purchase Cost</CLabel>
+                  </CCol>
+                <CCol xs="12" md="9">
+                    <CInputGroup>
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          {/* <CIcon name="cil-euro" /> */} PHP
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput id="input3-group1" name="input3-group1" placeholder=".." />
+                      <CInputGroupAppend>
+                        <CInputGroupText>.00</CInputGroupText>
+                      </CInputGroupAppend>
+                    </CInputGroup>
+                  </CCol>
+                </CFormGroup >
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Warranty</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                      <div className="controls">
+                      
+                        <CInputGroup>
+                          <CInput id="appendedInput" size="12" type="text" />
+                          <CInputGroupAppend>
+                            <CInputGroupText>months</CInputGroupText>
+                          </CInputGroupAppend>
+                        </CInputGroup>
+                        
+                      </div>
+                      </CCol>
+                    </CFormGroup>
+                    <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="select">Default Location</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select</option>
+                      <option value="1">Option #1</option>
+                      <option value="2">Option #2</option>
+                      <option value="3">Option #3</option>
+                    </CSelect>
+                  </CCol>
+                  
+                </CFormGroup>
+                
+                <CFormGroup row>
+                <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="textarea-input">Notes</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CTextarea 
+                      name="textarea-input" 
+                      id="textarea-input" 
+                      rows="2"
+                      placeholder="Content..." 
+                    />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="2" className="d-flex justify-content-sm-end">
+                    <CLabel htmlFor="text-input">Upload Image</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                  {/* <CInput type="date" id="date-input" name="date-input" placeholder="date" /> */}
+                  </CCol>
+                </CFormGroup>
+                <CCardFooter row>
+                <CCol md="12" className="d-flex justify-content-sm-end">
+                <CButton type="reset" size="sm" color="primary" className="mr-1">  Save</CButton>
+              
+                <CButton type="reset" size="sm" color="danger" className="mr-1"> Cancel</CButton>
+              </CCol>
+            </CCardFooter>
           </CCardBody>
         </CCard>
       </CCol>
