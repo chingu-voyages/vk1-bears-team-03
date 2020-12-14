f9c25db2128b03e6db42acc18496c4fe9454d90d
Aubrey Rose Gabisay <ARHgabisay@gmail.com>
2020-12-14 19:03:46 +0800

    merging recent changes

--- src/views/request/Requests.js
+++ src/views/request/Requests.js
@@ -10,127 +10,132 @@ import requestsData from "./RequestsData"
 
 
 const Requests = () => {
-    const [details, setDetails] = useState([])
-    const toggleDetails = (index) => {
-      const position = details.indexOf(index)
-      let newDetails = details.slice()
-      if (position !== -1) {
-        newDetails.splice(position, 1)
-      } else {
-        newDetails = [...details, index]
-      }
-      setDetails(newDetails)
-    }
-
-  const fields = [
-    { key: 'id'},
-    { key: 'user'},
-     'item_name', 'request_type', 'request_date',
-    {
-      key: 'show_details',
-      label: 'Actions',
-      _style: { width: '1%' },
-      sorter: false,
-      filter: false
-    }
-  ]
-
-  const [modal, setModal] = useState(false)
-  
-  const toggle = () => {
-    setModal(!modal);
+const [details, setDetails] = useState([])
+const toggleDetails = (index) => {
+  const position = details.indexOf(index)
+  let newDetails = details.slice()
+  if (position !== -1) {
+    newDetails.splice(position, 1)
+  } else {
+    newDetails = [...details, index]
   }
+  setDetails(newDetails)
+}
 
+const fields = [
+  {
+    key: 'select',
+    label: 'Select',
+    _style: { width: '1%'}
+  },
+  { key: 'id'},
+  { key: 'name'},
+   'serial_number', 'asset_tag','location', 'status',
+  {
+    key: 'show_details',
+    label: 'Actions',
+    _style: { width: '1%' },
+    sorter: false,
+    filter: false
+  }
+]
 const tableFilter = {
   label: 'Search',
   placeholder: 'Type here..',
 }
 
-  return (
-    <CDataTable
-      overTableSlot = { 
-      <CRow>
-        <CCol className="d-flex justify-content-sm-start">
-              <Search />
-              <Itemspage />
-          </CCol>
-          <CCol className="d-flex justify-content-sm-end">
-          {/* <Route render={({ history}) => ( */}
-            <Button label = "Create New"/>
-          {/* // )} /> */}
-          </CCol>
-          
-      </CRow>
-    }
-      items={requestsData}
-      fields={fields}
-      hover
-      pagination
-      scopedSlots = {{
-        // 'status':
-        //   (item)=>(
-        //     <td>
-        //       <CBadge color={getBadge(item.status)}>
-        //         {item.status}
-        //       </CBadge>
-        //     </td>
-        //   ),
-        'show_details':
-          (item, index)=>{
-            return (
-              <td className="py-2">
-                <CButton
-                  color="primary"
-                  variant="outline"
-                  shape="square"
-                  size="sm"
-                  onClick={()=>{toggleDetails(index)}}
-                >
-                  {details.includes(index) ? 'Hide' : 'Show'}
-                </CButton>
-              </td>
-              )
-          },
-        'details':
-            (item, index)=>{
-              return (
-              <CCollapse show={details.includes(index)}>
-                <CCardBody>
-                  <h4>
-                    {item.name}
-                  </h4>
-                  <p className="text-muted">Role: {item.user_role}</p>
-                  <CButton size="sm" color="primary" className="mr-1">
-                    Approve
-                  </CButton>
-                  <CButton size="sm" color="danger" className="mr-1" onClick={toggle}>Deny</CButton>
-                  <CButton size="sm" color="dark" className="mr-1">
-                    View Request Details
-                  </CButton>
-                  <CModal
-                    show={modal}
-                    onClose={toggle}
-                  >
-                    <CModalHeader closeButton>Deny Request</CModalHeader>
-                    <CModalBody>
-                      Are you sure you want to deny the request?
-                    </CModalBody>
-                    <CModalFooter>
-                      <CButton color="primary">Yes</CButton>{' '}
-                      <CButton
-                        color="secondary"
-                        onClick={toggle}
-                      >Cancel</CButton>
-                    </CModalFooter>
-                  </CModal>
-                </CCardBody>
-              </CCollapse>
-            )
-          }
-      }}
-    />
-  )
+const getBadge = (status)=>{
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
   }
+}
 const [modal, setModal] = useState(false)
   
-  export default Requests
+const toggle = () => {
+  setModal(!modal);
+}
+
+return (
+  <>
+  <CDataTable
+    items={requestsData}
+    header
+    fields={fields}
+    tableFilter={tableFilter}
+    itemsPerPageSelect
+    itemsPerPage={5}
+    hover
+    pagination
+  
+    scopedSlots = {{
+    'status':
+      (item)=>(
+        <td>
+          <CBadge color={getBadge(item.status)}>
+            {item.status}
+          </CBadge>
+        </td>
+      ),
+    'show_details':
+      (item, index)=>{
+        return (
+          <td className="py-2">
+            <CButton
+              color="primary"
+              variant="outline"
+              shape="square"
+              size="sm"
+              onClick={()=>{toggleDetails(index)}}
+            >
+              {details.includes(index) ? 'Hide' : 'Show'}
+            </CButton>
+          </td>
+          )
+      },
+    'details':
+        (item, index)=>{
+          return (
+          <CCollapse show={details.includes(index)}>
+            <CCardBody>
+              <h4>
+                {item.name}
+              </h4>
+              <p className="text-muted">Role: {item.user_role}</p>
+              <CButton size="sm" color="info" className="mr-1">
+                Approve
+              </CButton>
+              <CButton size="sm" color="danger" className="mr-1" onClick={toggle}>Deny</CButton>
+              <CModal
+                show={modal}
+                onClose={toggle}
+              >
+                <CModalHeader closeButton>Delete Request</CModalHeader>
+                <CModalBody>
+                  Are you sure you want to delete Request?
+                </CModalBody>
+                <CModalFooter>
+                  <CButton color="primary">Yes</CButton>{' '}
+                  <CButton
+                    color="secondary"
+                    onClick={toggle}
+                  >Cancel</CButton>
+                </CModalFooter>
+              </CModal>
+            </CCardBody>
+          </CCollapse>
+        )
+      }
+      
+  }}
+      
+  />
+</>
+)
+
+}
+
+export default Requests
